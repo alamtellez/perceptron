007901f84b23ff93a8650e3abb0b52c355149b1c
Arturo Alam Téllez <a01205569@itesm.mx>
2018-11-10 00:44:42 +0000

    Parsing

--- perceptron.py
+++ perceptron.py
@@ -4,4 +4,18 @@
 # Alam Tellez
 # Fermin Moreno
 
-print("Hello World!")
+if __name__ == "__main__":
+    
+    dimensionality = int(input())
+    train_size = int(input())
+    test_size = int(input())
+    train_set = []
+    test_set = []
+    for _ in range(train_size):
+        train_set.append(list(map(int,input().replace(" ", "").split(","))))
+    for _ in range(test_size):
+        test_set.append(list(map(int,input().replace(" ", "").split(","))))
+    # train_set = list(map(int, train_set))
+    # test_set = list(map(int, test_set))
+    
+    print(train_set, test_set)
