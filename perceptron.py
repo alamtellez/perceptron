c0647a59f31b2f6773efe6e588e5d34261880884
atellez08 <a01205569@itesm.mx>
2018-11-15 04:36:49 +0000

    Idk

--- perceptron.py
+++ perceptron.py
@@ -16,49 +16,49 @@
 2, 2
 0, 0
 '''
+
 import random
 import math
 
-
 def activation_function(vals, weights, threshold):
-	result = 0
-	for i in range(len(vals)):
-	    #print(vals[i], weights[i])
-		result += vals[i] * weights[i]
-	#print(result)
-	if result > threshold:
-		return 1
-	else:
-		return 0
+    result = 0
+    for i in range(len(vals)):
+        #print(vals[i], weights[i])
+        result += vals[i] * weights[i]
+    #print(result)
+    if result > threshold:
+        return 1
+    else:
+        return 0
 
-def update_weights(train_vals, weights, treshold):
-	last = train_vals[-1]
-	a_function_result  = activation_function(train_vals[:-1], weights, treshold)
-	#print(a_function_result)
-	error = last - a_function_result
-	for i in range(len(weights)):
-		weights[i] += error * train_vals[i]
-	return error
+def update_weights(vals, weights, threshold):
+    output = vals[-1]
+    a_function_result = activation_function(vals[0:-1], weights, threshold)
+    #print(a_function_result)
+    error = output - a_function_result
+    for i in range(len(weights)):
+        weights[i] += error * vals[i]
+    return (error, weights)
 
-def start_weights(weights, dimensionality):
-    # Setting small random numbers to the initial weights
+# The number of weights is defined by the dimensionality of the input
+def starting_weights(weights, dimensionality):
     for i in range(dimensionality):
-    	weights.append(0)
-    
-def train_newtork(epochs, train_set, threshold, weights):
-    for _ in range(epochs):
+        # Setting small random numbers to the initial weights
+        weights.append(random.random())
+    return weights
+
+def train_newtork(train_set, weights, epochs, threshold):
+    for i in range(epochs):
         error = 0
-        for val in train_set:
-    
-            aux_error = update_weights(val, weights, threshold)
+        for j in range(len(train_set)):
+            aux_error, weights = update_weights(train_set[j], weights, threshold)
             error += pow(aux_error, 2)
-        # This is just to prevent overfitting
-        if error == 0:
-            break
-        
+            # This is just to prevent overfitting
+            if error == 0:
+                break
     #print(error)
     #print(weights)
-    return error
+    return (error, weights)
 
 def test_network(test_set, error, weights, threshold):
     if error >= 1 or len(test_set) == 0:
@@ -66,24 +66,33 @@ def test_network(test_set, error, weights, threshold):
     else:
         for vals in test_set:
             print(activation_function(vals, weights, threshold))
-        
+
 if __name__ == "__main__":
-    
+
     dimensionality = int(input())
     train_size = int(input())
     test_size = int(input())
     train_set = []
     test_set = []
-    weights = [] 
-    for _ in range(train_size):
-        train_set.append(list(map(float,input().rstrip('\n').rstrip('\r').replace(" ", "").split(","))))
-    for _ in range(test_size):
-        test_set.append(list(map(float,input().rstrip('\n').rstrip('\r').replace(" ", "").split(","))))
-    
-    
-    threshold = random.randrange(0,2)
-    start_weights(weights, dimensionality)
+    for i in range(train_size):
+        parsed_line = input().rstrip('\n').rstrip('\r').replace(" ", "").split(",")
+        for i in range(len(parsed_line)):
+            parsed_line[i] = float(parsed_line[i])
+        train_set.append(parsed_line)
+    for i in range(test_size):
+        parsed_line = input().rstrip('\n').rstrip('\r').replace(" ", "").split(",")
+        for i in range(len(parsed_line)):
+            parsed_line[i] = float(parsed_line[i])
+        test_set.append(parsed_line)
+    # train_set = list(map(int, train_set))
+    # test_set = list(map(int, test_set))
+
+    #print(train_set[0][0:-1])
+
+    weights = starting_weights([], dimensionality)
     epochs = 100
-    error = train_newtork(epochs, train_set, threshold, weights)
-    
+    threshold = random.randrange(0,2)
+    error = 0
+    error, weights = train_newtork(train_set, weights, epochs, threshold)
+
     test_network(test_set, error, weights, threshold)
